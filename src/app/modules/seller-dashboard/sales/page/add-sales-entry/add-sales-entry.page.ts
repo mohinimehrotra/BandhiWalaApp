154b70b759b44482254a8adb80e3623b59f2a577
aabharathore0603 <69072330+aabharathore0603@users.noreply.github.com>
2021-09-15 17:48:48 +0530

    single & bulk entry

--- src/app/modules/seller-dashboard/sales/page/add-sales-entry/add-sales-entry.page.ts
+++ src/app/modules/seller-dashboard/sales/page/add-sales-entry/add-sales-entry.page.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-sales-entry.page.scss'],
 })
 export class AddSalesEntryPage implements OnInit {
-
-  constructor() { }
-
+  showSingleEntry = true;
+  showBulkEntry = false;
+  isChecked=false;
+  numbers = []
+  constructor() {
+    this.numbers = Array(20).fill(1).map((x,i)=>i);
+   }
+  
   ngOnInit() {
+  }
+  viewChanged(ev){
+    if(ev.detail.value === 'single-entry'){
+      this.showSingleEntry = true;
+      this.showBulkEntry = false;
+    }
+    if(ev.detail.value === 'bulk-entry'){
+      this.showBulkEntry = true;
+      this.showSingleEntry = false;
+    }
+  }
+
+  toggleState(){
+    this.isChecked=true;
   }
 
 }
