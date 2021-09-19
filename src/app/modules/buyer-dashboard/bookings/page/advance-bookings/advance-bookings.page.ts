1fdb6ace50764cc694ff8093c63cc14eda902eba
aabharathore0603 <69072330+aabharathore0603@users.noreply.github.com>
2021-09-19 16:23:21 +0530

    buyer-home and advance-booking

--- src/app/modules/buyer-dashboard/bookings/page/advance-bookings/advance-bookings.page.ts
+++ src/app/modules/buyer-dashboard/bookings/page/advance-bookings/advance-bookings.page.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./advance-bookings.page.scss'],
 })
 export class AdvanceBookingsPage implements OnInit {
-
+  showActive = true;
+  showRequested = false;
   constructor() { }
 
   ngOnInit() {
   }
-
+  viewChange(ev){
+    if(ev.detail.value === 'active'){
+      this.showActive = true;
+      this.showRequested = false;
+    }
+    if(ev.detail.value === 'requested'){
+      this.showRequested = true;
+      this.showActive = false;
+    }
+  }
 }
