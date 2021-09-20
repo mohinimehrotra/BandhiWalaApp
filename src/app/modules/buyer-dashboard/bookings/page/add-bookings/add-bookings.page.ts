5d84a95440caf68e3ccb4b17c6f227d92a86d3d5
aabharathore0603 <69072330+aabharathore0603@users.noreply.github.com>
2021-09-20 19:33:12 +0530

    add more product and date time field added

--- src/app/modules/buyer-dashboard/bookings/page/add-bookings/add-bookings.page.ts
+++ src/app/modules/buyer-dashboard/bookings/page/add-bookings/add-bookings.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-add-bookings',
@@ -7,9 +8,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddBookingsPage implements OnInit {
 
-  constructor() { }
+  addBookingForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.buildForms();
+  }
+
+  buildForms(){
+    this.addBookingForm = this.formBuilder.group({
+      seller: [],
+      date: [],
+      time: [],
+      products: this.formBuilder.array([])
+    });
+
+    this.loadOneProductGroupInProducts();
+  }
+
+  get products(): FormArray{
+    return this.addBookingForm.get('products') as FormArray;
+  }
+
+  addProduct(){
+    const formControl = this.formBuilder.group({
+      productName: [''],
+      quantity: [''],
+      unit: [''],
+      rate: [''],
+      price: ['']
+
+    });
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+    (<FormArray>this.addBookingForm.get('products')).push(formControl);
+  };
+
+  loadOneProductGroupInProducts(){
+    const formControl = this.formBuilder.group({
+      productName: [''],
+      quantity: [''],
+      unit: [''],
+      rate: [''],
+      price: ['']
+
+    });
+
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+    (<FormArray>this.addBookingForm.get('products')).push(formControl);
+  }
+
+  removeProduct(index){
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+    this.products.removeAt(index);
+  };
+
+  onSave(){
+    console.log(this.addBookingForm.value);
+
   }
 
 }
