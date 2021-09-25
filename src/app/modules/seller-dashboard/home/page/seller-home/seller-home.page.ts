12072dab13f80d123e94e5f96f4a1d5b98e917ce
devdutt-dikshit <devdutt.technomize@gmail.com>
2021-09-25 20:22:08 +0530

    seller dashboard integrated

--- src/app/modules/seller-dashboard/home/page/seller-home/seller-home.page.ts
+++ src/app/modules/seller-dashboard/home/page/seller-home/seller-home.page.ts
@@ -1,5 +1,7 @@
 /* eslint-disable no-trailing-spaces */
 import { Component, OnInit } from '@angular/core';
+import { BOOKING_PENDING } from 'src/app/core/constants/storage.constant';
+import { StorageService } from 'src/app/core/services/storage.service';
 import { SellerDashboardService } from '../../../services/seller-dashboard.service';
 @Component({
   selector: 'app-seller-home',
@@ -8,13 +10,50 @@ import { SellerDashboardService } from '../../../services/seller-dashboard.servi
 })
 export class SellerHomePage implements OnInit {
 
+  dashboardStats = {};
+  showEmpty= true;
+  customErrorMessage: String;
+  pendingItems = [];
+  showPendingItems = false;
+  userData = {};
+
   constructor(
-    private sellerDashboardService: SellerDashboardService
+    private sellerDashboardService: SellerDashboardService,
+    private storageService: StorageService
+
   ) { }
 
   ngOnInit() {
-    this.sellerDashboardService.toggleMenuView(true);
-    
+    this.sellerDashboardService.toggleMenuView(true); 
+    this.onfetchDashboardStats();
+    this.onFetchBookings(BOOKING_PENDING);
+    this.userData = this.storageService.getuserData()
+  }
+
+  onfetchDashboardStats(){
+    this.sellerDashboardService.fetchDashbordStats().subscribe((response: any) => {
+      console.log(response);
+      this.dashboardStats = response;
+      console.log(this.dashboardStats)
+    }, error => {
+      this.customErrorMessage = error;
+    });
+  }
+
+  onFetchBookings(status){
+    this.sellerDashboardService.fetchBooking({ status }).subscribe((response: any) => {
+      console.log(response);
+      if(status === BOOKING_PENDING){
+        this.pendingItems = response.data;
+        if(this.pendingItems.length){
+          this.showEmpty= false;
+          this.showPendingItems = true;
+        }
+        console.log(this.pendingItems);
+      }
+    }, error => {
+      this.customErrorMessage = error;
+    });
   }
 
 }
